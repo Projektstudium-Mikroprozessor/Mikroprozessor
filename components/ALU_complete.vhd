2dbfa6128000300465bebbab3bfe2e086c20748e
Lisa-Marie <lisamarie.mai@icloud.com>
2019-11-26 22:41:24 +0100

    added mux_2_1 to structure of ALU_complete

--- components/ALU_complete.vhd
+++ components/ALU_complete.vhd
@@ -4,8 +4,11 @@ use IEEE.numeric_std.all;
 
 entity ALU_complete is 
     port (
-        daten1          : in std_logic;
-        daten2          : in std_logic;
+        clk_external    : in std_logic;
+        ALUscr          : in std_logic;
+        ALUOp           : in std_logic_vector(1 downto 0);
+        daten1          : in std_logic_vector(31 downto 0);
+        daten2          : in std_logic_vector(31 downto 0);
         vz_erweiterung  : in std_logic_vector(31 downto 0);
         func            : in std_logic_vector(5 downto 0);
         zero            : out std_logic;
@@ -14,8 +17,8 @@ entity ALU_complete is
 end ALU_complete;
 
 architecture structure of ALU_complete is   
-    signal mux_out          : std_logic;
+    signal mux_out          : std_logic_vector(31 downto 0);
     signal alu_cntrl_out    : std_logic_vector(3 downto 0);
     begin
-        
-end architecture;
+        mux_alu :   entity work.mux_2_1 port map(clk => clk_external, e0 => daten1, e1 => daten2, s => ALUscr, a => mux_out);
+end structure;
